f5ae1fa6da6a15c0f7e7bb7a504f8e6765b92928
Nick Clark <limeman@gmail.com>
2025-10-13 13:59:10 -0400

    Fixing this up

--- src/app/main.js
+++ src/app/main.js
@@ -1,7 +1,7 @@
-import { jsx as _jsx } from 'react/jsx-runtime';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-ReactDOM.createRoot(document.getElementById('root')).render(
-  _jsx(React.StrictMode, { children: _jsx(App, {}) })
+import { jsx as _jsx } from "react/jsx-runtime";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+ReactDOM.createRoot(document.getElementById("root")).render(
+  _jsx(React.StrictMode, { children: _jsx(App, {}) }),
 );
