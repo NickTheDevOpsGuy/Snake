f19e864a8ebc2672fa73ddb141e3ff3dc0db4caf
Nick Clark <limeman@gmail.com>
2025-10-13 14:53:51 -0400

    Fixing this up

--- src/app/App.js
+++
@@ -1,7 +0,0 @@
-import { jsx as _jsx } from 'react/jsx-runtime';
-// src/SnakeApp.tsx
-import SnakeCanvas from './components/SnakeCanvas';
-import './styles/App.css';
-export default function SnakeApp() {
-  return _jsx(SnakeCanvas, {});
-}
